a050503e4dfe8e5edc3bc4392632edfb4550714e
Sumanth Padakanti <sumanthpadakanti.psr@gmail.com>
2026-03-04 18:38:33 -0500

    create http server

--- node-farm/starter/index.js
+++ node-farm/starter/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-
+const http = require("http");
 fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
   console.log(data);
 });
@@ -7,4 +7,12 @@ fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
 const text = `some text added to writeFile`;
 fs.writeFile("./txt/writeFile.txt", text, "utf-8", (err) => {
   console.log(err);
+});
+
+const server = http.createServer((req, res) => {
+  res.end(`Hello from server`);
+});
+
+server.listen("4000", "127.0.0.1", () => {
+  console.log("start listenting to request");
 });
