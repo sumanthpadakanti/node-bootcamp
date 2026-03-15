dad3bcb6d312902120f4f8e4bd71fd0bd2ac9b39
Sumanth Padakanti <sumanthpadakanti.psr@gmail.com>
2026-03-15 17:15:46 -0400

    update http methods in app.js

--- natours/app.js
+++ natours/app.js
@@ -15,12 +15,12 @@ app.use(express.json());
 // app.post('/', (req, res) => {
 //   res.status(404).send(`you can't use post on this url`);
 // });
-
-app.get('/api/v1/tours', getAllTours);
-app.post('/api/v1/tours', createTour);
-app.get('/api/v1/tours/:id', getTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 app.listen(3000, () => {
   console.log('Server is listening on 3000');
 });
