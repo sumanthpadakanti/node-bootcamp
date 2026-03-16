84a707c21441b45b3c553a5a33a6fc589217db7e
Sumanth Padakanti <sumanthpadakanti.psr@gmail.com>
2026-03-15 21:21:57 -0400

    add controllers and route files for tours and users

---
+++ natours/routes/userRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userHandler');
+const router = express.Router();
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+module.exports = router;
